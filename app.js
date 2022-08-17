def8152d90457ad3f52da90a9e013d6d8e78b7f1
Kyubum Jang <jkb2221@gmail.com>
2022-08-17 09:35:17 +0900

    feat: GET 요청 테스트

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ app.listen(3000, function () {
 	console.log('start nodejs!!');
 });
 
+/* 
+  static 파일(JS, CSS, image 파일)들을 public 폴더에 두고 관리할 때
+  express에 등록하는 절차
+*/
+app.use(express.static('public'));
+
 // url routing
 app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/main.html');
+	// res.send('<h1>hi, express</h1>');
+});
+
+app.get('/main', function (req, res) {
 	res.sendFile(__dirname + '/public/main.html');
 	// res.send('<h1>hi, express</h1>');
 });
---
+++ public/main.js
@@ -0,0 +1,1 @@
+console.log('main.js 로드');
